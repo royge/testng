73e24c38c486580d14fd858edad1f60d710983eb
Roy Evangelista <royevangelista@gmail.com>
2021-06-16 14:35:41 +0800

    Update firestore client constructor

--- firetest/firetest.go
+++ firetest/firetest.go
@@ -10,7 +10,7 @@ import (
 
 // StoreClient tries to create firestore client for testing.
 // It also delete the documents in the collection after the tests.
-func StoreClient(t *testing.T, projectID, collection string) (
+func StoreClient(t *testing.T, projectID string, collections ...string) (
 	client *firestore.Client,
 ) {
 	t.Helper()
@@ -21,26 +21,29 @@ func StoreClient(t *testing.T, projectID, collection string) (
 	}
 
 	t.Cleanup(func() {
-		t.Logf("[INFO] start cleaning up `%v` firestore collection...", collection)
 		ctx := context.Background()
-		iter := client.Collection(collection).DocumentRefs(ctx)
-		for {
-			doc, err := iter.Next()
-			if err == iterator.Done {
-				break
-			}
-			if err != nil {
-				t.Fatalf("error iterating to next document: %v", err)
-			}
-			t.Logf("[INFO] deleting document `%v` ...", doc.ID)
-			_, err = doc.Delete(ctx)
-			if err != nil {
-				t.Fatalf("error deleting document: %v", err)
-			}
-			t.Logf("[INFO] done deleting document `%v` ...", doc.ID)
-		}
 
-		t.Logf("[INFO] done cleaning up `%v` firestore collection", collection)
+		for _, collection := range collections {
+			t.Logf("[INFO] start cleaning up `%v` firestore collection...", collection)
+			iter := client.Collection(collection).DocumentRefs(ctx)
+			for {
+				doc, err := iter.Next()
+				if err == iterator.Done {
+					break
+				}
+				if err != nil {
+					t.Fatalf("error iterating to next document: %v", err)
+				}
+				t.Logf("[INFO] deleting document `%v` ...", doc.ID)
+				_, err = doc.Delete(ctx)
+				if err != nil {
+					t.Fatalf("error deleting document: %v", err)
+				}
+				t.Logf("[INFO] done deleting document `%v` ...", doc.ID)
+			}
+
+			t.Logf("[INFO] done cleaning up `%v` firestore collection", collection)
+		}
 		client.Close()
 	})
 
